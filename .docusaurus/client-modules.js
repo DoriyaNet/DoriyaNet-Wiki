44cb43270ab48b58e9947c44c70c96850cee662d
rtxneko <157133282+rtxneko@users.noreply.github.com>
2025-12-19 05:42:21 +0000

    add i18n support

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/workspaces/DoriyaNet-Wiki/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/workspaces/DoriyaNet-Wiki/node_modules/infima/dist/css/default/default.css"),
+  require("/workspaces/DoriyaNet-Wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/workspaces/DoriyaNet-Wiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/workspaces/DoriyaNet-Wiki/src/css/custom.css"),
+];
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'DoriyaNet Wiki',
+  tagline: 'a stupid wiki',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -22,8 +22,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'DoriyaNet', // Usually your GitHub org/user name.
+  projectName: 'DoriyaNet', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -31,8 +31,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-cn',
+    locales: ['zh-cn'],
   },
 
   presets: [
@@ -75,7 +75,7 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'DoriyaNet',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -111,16 +111,12 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: 'Telegram Channel',
+              href: 'https://t.me/DoriyaNet',
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
+              label: 'Telegram Disscuss',
+              href: 'https://t.me/DoriyaNetDiscuss',
             },
           ],
         },
@@ -138,7 +134,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} DoriyaNet`,
     },
     prism: {
       theme: prismThemes.github,
